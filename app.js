e8db1e81d98a94f4813fd6a676f22ddc2cccdad2
Seunghui Jang <seunghuijang@Seunghuiui-MacBookPro.local>
2019-05-06 16:02:46 -0400

    ss

---
+++ app.js
@@ -0,0 +1,21 @@
+let express = require('express'),
+    path = require('path');
+var app = express();
+let server = require('http').Server(app);
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
+
+app.get('/blog.html', function(req, res,next){
+    res.sendFile(path.join(__dirname+"/blog.html"));
+});
+
+app.post('/contact', function(req, res, next){
+
+});
+server.listen('8000', function() {
+    console.log("App is running on port 8000");
+});
